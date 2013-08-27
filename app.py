62b71ffbc0653df12ea3489aff2acbcd6ea539d5
Rishi Mukherjee <thethunder666@gmail.com>
2013-08-28 01:12:14 +0530

    Added validation of instamojo tweets.

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import logging
 import getpass
 import requests
 from config import *
-from sys import argv
+import re
+from sys import argv, exit
 from requests_oauthlib import OAuth1
 from urlparse import parse_qs
 
@@ -23,8 +24,14 @@ class tweetMojo():
        return [tweet['text'] for tweet in req.json()]
    
    def parse_tweet_for_instamojo_offer(self, tweet):
-       formdata = {}
+       offer_key_value = dict(re.findall(r'(\S+)=(".*?"|\S+)', tweet[1:]))
+       return offer_key_value
        
+   def check_tweet_is_instamoffer(self, tweet):
+       return tweet.lower().startswith("instamoffer")
+       
+   def check_offer_parameters(self, offer_key_value):
+       return "title" in offer_key_value and "desc" in offer_key_value and "file" in offer_key_value and ("usd" in offer_key_value or "inr" in offer_key_value)
    
 if __name__ == '__main__':
     logging.basicConfig(filename='debug.log', level=logging.DEBUG)
@@ -35,5 +42,16 @@ if __name__ == '__main__':
     logging.info('username: %s' % username)
     my_mojo = tweetMojo()
     my_mojo.new_twitter_user(username)
-    print my_mojo.get_n_tweets(username)
+    latest_tweet = my_mojo.get_n_tweets(username)[0]
+    if my_mojo.check_tweet_is_instamoffer(latest_tweet):
+        offer_details = my_mojo.parse_tweet_for_instamojo_offer(latest_tweet)
+        if my_mojo.check_offer_parameters(offer_details):
+            print offer_details
+        else:
+            print "The tweet format does not match the specified format."
+            exit()
+    else:
+        print "The tweet is not about an instamojo offer."
+        exit()
+        
     
